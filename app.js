fdadd33d081cf778ab9ea0c1b9850eeead7afa86
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-09-18 10:49:06 -0400

    added alert after form submission, added top border on section titles, removed an unused library

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let sheetAsJSON =
 	'https://spreadsheets.google.com/feeds/list/13ctCBuBlGwJCzYjHmGPistVh0hm6UPVSR6X1FHwafjA/od6/public/values?alt=json'; 
 
 $.ajax({ url: sheetAsJSON }).then((data) => {
-	// console.log(data)
+	// console.log('this is data: ', data)
 	const projects = data.feed.entry.map((project) => {
 		return {
 			image: project.gsx$image.$t,
@@ -21,11 +21,12 @@ $.ajax({ url: sheetAsJSON }).then((data) => {
 		};
 	});
 	render(projects);
-	// console.log(projects);
+	// console.log('this is projects (array): ', projects);
 });
 
 const render = (array) => {
 	array.forEach((project) => {
+        // console.log('this is project - ', project)
 		$('.project-cards').append(
 			`<div class="card" style="width: 18rem;">
             <img src="${project.image}" class="card-img-top" alt="${project.alttext}">
@@ -41,18 +42,17 @@ const render = (array) => {
 	});
 };
 
-const userInput = [];
+
+
 $('form').on('submit', (event) => {
-	event.preventDefault();
-	const inputText = $('input[type="email"]').val();
-	/*reference element, reference property type, reference value(text)*/
-	console.log('this is inputText - ', inputText);
-	userInput.push(inputText);
-	console.log('this is the userInput array - ', userInput);
+    event.preventDefault();
+    alert("Thanks for the message! I will be in touch within 48 hours.")
 	$(event.currentTarget).trigger('reset');
 });
 
-// googled "how to automatically collapse bootstrap navbar" and used this article as a starting point but wrote my onclick method moreso in the format we learned and chose to listen to nav-link because i want nav to close specifically when user clicks on a link  https://stackoverflow.com/questions/14248194/close-responsive-navbar-automatically
+
+// googled "how to automatically collapse bootstrap navbar" and used this article as a starting point but wrote my onclick method more so in the format we learned in class. i chose to listen to nav-link because i want nav to close specifically when user clicks on a link  https://stackoverflow.com/questions/14248194/close-responsive-navbar-automatically
+
 $('.nav-link').on('click', () => {
 	$('.navbar-collapse').collapse('hide');
 });
